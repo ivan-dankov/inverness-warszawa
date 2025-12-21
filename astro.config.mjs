b8514356d1f7830f10413c688f63583526189ff6
ivan-dankov <ivandankovdigital@gmail.com>
2025-12-21 13:24:20 +0100

    Remove Vercel adapter for static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
   integrations: [
     react(),
     tailwind({
